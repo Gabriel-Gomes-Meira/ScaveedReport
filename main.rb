8a27bdef905f4d8064cf745d08f21a49258cc250
Gabriel Gomes Meira <62515106+Gabriel-Gomes-Meira@users.noreply.github.com>
2022-07-09 23:24:54 -0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -10,8 +10,8 @@ client = Mongo::Client.new([ '127.0.0.1:27017' ],
                            :database => 'mining_net_development')
 
 user = client[:users].find({}).first
-$token = user[:token]
-$chatid = user[:chat_id]
+$token = user[:telegram][:token]
+$chatid = user[:telegram][:chat_id]
 
 while true do
   db = client.database
