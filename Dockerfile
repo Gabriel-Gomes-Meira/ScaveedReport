f22e12b23946608845572f52118d5e1771525944
Gabriel Gomes Meira <62515106+Gabriel-Gomes-Meira@users.noreply.github.com>
2022-10-09 16:00:10 -0300

    Em Scraper, o Distinguer não existe

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 # syntax=docker/dockerfile:1
 FROM ruby:3.1.2
-RUN apt-get update -qq && apt-get install -y nodejs sqlite3
+RUN apt-get update -qq && apt-get install -y sqlite3
 WORKDIR /myapp
 COPY Gemfile /myapp/Gemfile
 COPY Gemfile.lock /myapp/Gemfile.lock
 RUN bundle install
+RUN gem install sqlite3
 
 # Add a script to be executed every time the container starts.
 #COPY entrypoint.sh /usr/bin/
--- main.rb
+++ main.rb
@@ -22,35 +22,36 @@ if user
 
       current_state = scrap_value(scrap_items(page, ele[:element_indentifier]),
                                     "inner_html")
-      
 
-      if client[:reports].where{(listen_id: ele[:id) & (current_state: current_state)} != nil
+      # byebug
+      if client[:reports].where(listen_id: ele[:id], current_state: current_state).first == nil
+        client.transaction do
+          report = {:listen_id => ele[:id],
+                    :current_state => current_state,
+                    :at => Time.now}
 
-        report = {:listen_id => ele[:_id],
-                :current_state => current_state,
-                :at => Time.now}
-
-        #notificar no telegram
-        begin
-          model = client[:notification_models].where(id: ele[:notification_model_id]).first
-          if !model.nil?            
-            notificar_telegram(mount_message(client[:items].where(notification_model_id: model[:id]), model[:message]),
-            !model[:message].rindex("<img>").nil?)
+          #notificar no telegram
+          begin
+            model = client[:notification_models].where(id: ele[:notification_model_id]).first
+            if !model.nil?
+              notificar_telegram(mount_message(client[:items].where(notification_model_id: model[:id]), model[:message]),
+                                 !model[:message].rindex("<img>").nil?)
+            end
+          rescue StandardError => e
+            logs.insert({:message_log => e.full_message, :at => Time.new})
+          rescue SyntaxError => e
+            logs.insert({:message_log => e.full_message, :at => Time.new})
           end
-        rescue StandardError => e
-          logs.insert({:message_log => e.full_message, :at => Time.new})
-        rescue SyntaxError => e
-          logs.insert({:message_log => e.full_message, :at => Time.new})
-        end
 
-        #executar tarefa associada
-        model_task = client[:model_tasks].where(id: ele[:model_task_id]).first
-        if !model_task.nil?
-          client[:queued_tasks].insert(model_task.except(:id))
-        end
+          #executar tarefa associada
+          model_task = client[:model_tasks].where(id: ele[:model_task_id]).first
+          if !model_task.nil?
+            client[:queued_tasks].insert(model_task.except(:id, :created_at))
+          end
 
-        ##Relatar mudança no banco de dados
-        client[:reports].insert(report)
+          ##Relatar mudança no banco de dados
+          client[:reports].insert(report)
+        end
 
       end
       
